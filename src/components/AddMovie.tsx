c9686b7b104e3722c6404712be44d86952eda2cb
Judit Horvath <hjudit@protonmail.com>
2024-02-23 11:09:01 +0100

    Minimal styling

--- src/components/AddMovie.tsx
+++ src/components/AddMovie.tsx
@@ -26,33 +26,44 @@ const AddMovie = ({ onAddMovie }: NewMovieProps) => {
   return (
     <section>
       <form id="movieForm" onSubmit={(e) => handleSubmit(e)}>
-        <label className="" htmlFor="title">
-          Title:
-        </label>
-        <input type="text" name="title" id="title" required />
-        <label className="" htmlFor="rating">
-          Rating:
-        </label>
-        <input type="range" name="rating" id="rating" min="0" max="5" step="1" />
-        <label className="" htmlFor="genre">
-          Genre:
-        </label>
-
-        <select name="genre" id="genre">
-          <option value="Comedy">Comedy</option>
-          <option value="Thriller">Thriller</option>
-          <option value="Drama">Drama</option>
-        </select>
-        <label className="" htmlFor="description">
-          Description:
-        </label>
-        <textarea name="description" id="description"></textarea>
-        <button type="button" id="clearButton">
-          Clear
-        </button>
-        <button type="submit" id="addButton">
-          Add
-        </button>
+        <div className="propertyDiv">
+          <label className="" htmlFor="title">
+            Title:
+          </label>
+          <input type="text" name="title" id="title" required />
+        </div>
+        <div className="propertyDiv">
+          <label className="" htmlFor="rating">
+            Rating:
+          </label>
+          <input type="range" name="rating" id="rating" min="0" max="5" step="1" />
+        </div>
+        <div className="propertyDiv">
+          <label className="" htmlFor="genre">
+            Genre:
+          </label>
+          <select name="genre" id="genre">
+            <option value="Comedy">Comedy</option>
+            <option value="Thriller">Thriller</option>
+            <option value="Drama">Drama</option>
+          </select>
+        </div>
+        <div className="propertyDiv">
+          <label className="" htmlFor="description">
+            Description:
+          </label>
+          <div className="textAreaAndButtonsDiv">
+            <textarea name="description" id="description" rows={6}></textarea>
+            <div className="buttonDiv">
+              <button type="button" id="clearButton">
+                Clear
+              </button>
+              <button type="submit" id="addButton">
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
       </form>
     </section>
   );
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -7,10 +7,12 @@ type MovieCardProps = {
 
 const MovieCard = ({ movie, onDeleteMovie }: MovieCardProps) => {
   return (
-    <div onClick={() => onDeleteMovie(movie.id)}>
-      <h1>{movie.title}</h1>
-      <span>{movie.rating}</span>
-      <p>{movie.genre}</p>
+    <div className="movieCard" onClick={() => onDeleteMovie(movie.id)}>
+      <div className="movieTitleAndRating">
+        <h1>{movie.title}</h1>
+        <span>{movie.rating}/5</span>
+      </div>
+      <p className="genre">{movie.genre}</p>
       <p>{movie.description}</p>
     </div>
   );
